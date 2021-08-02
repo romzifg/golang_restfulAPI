d3da33c463f4b2e90c4afe06d9cdadb0590019db
Romzi Farhan <romzifg@gmail.com>
2021-08-02 23:28:38 +0700

    create entity campaign

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,7 @@
 package handler
+
+// 1. Menangkap parameter
+// 2. Handler mengirim parameter ke service
+// 3. Service menentukan repository/method yg digunakan
+// 4. Repository : GetAll, GetByUserID
+// 5. Repository mengakses db
