5eee6a072524516f7ab1c9b4ff5f9d022e7bdce5
Romzi Farhan <romzifg@gmail.com>
2021-08-03 16:55:56 +0700

    create campaign & images

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"bwa_startup/auth"
+	"bwa_startup/campaign"
 	"bwa_startup/handler"
 	"bwa_startup/helper"
 	"bwa_startup/user"
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -26,6 +28,18 @@ func main() {
 	}
 
 	userRepository := user.NewRepository(db)
+	campaignRepository := campaign.NewRepository(db)
+
+	campaigns, err := campaignRepository.FindByUserID(1)
+	fmt.Println(len(campaigns))
+	for _, campaign := range campaigns {
+		fmt.Println(campaign.Name)
+		if len(campaign.CampaignImages) > 0 {
+			fmt.Println(len(campaign.CampaignImages))
+			fmt.Println(campaign.CampaignImages[0].FileName)
+		}
+	}
+
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
