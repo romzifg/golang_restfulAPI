4cb88591189fbdb76747c211f24b47c1d19143ca
Romzi Farhan <romzifg@gmail.com>
2021-08-03 19:11:46 +0700

    create campaign service

--- campaign/service.go
+++ campaign/service.go
@@ -1,1 +1,31 @@
 package campaign
+
+type Service interface {
+	GetCampaigns(userID int) ([]Campaign, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetCampaigns(userID int) ([]Campaign, error) {
+	if userID != 0 {
+		campaings, err := s.repository.FindByUserID(userID)
+		if err != nil {
+			return campaings, err
+		}
+
+		return campaings, nil
+	}
+
+	campaings, err := s.repository.FindAll()
+	if err != nil {
+		return campaings, err
+	}
+
+	return campaings, nil
+}
