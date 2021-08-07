e90a0f16a50dc71f393b13699de941ff93cf6351
Romzi Farhan <romzifg@gmail.com>
2021-08-07 17:56:00 +0700

    make folder transaction & add entity

---
+++ transaction/entity.go
@@ -0,0 +1,14 @@
+package transaction
+
+import "time"
+
+type Transaction struct {
+	ID         int 		 `json:"id"`
+	CampaignID int 		 `json:"campaign_id"`
+	UserID     int 		 `json:"user_id"`
+	Amount     int 		 `json:"amount"`
+	Status     string 	 `json:"status"`
+	Code       string 	 `json:"code"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
